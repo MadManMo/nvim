cbe23e2b5a844b7c3ba3d21121df35219ab640a6
Mad Man Mo <140839348+MadManMo@users.noreply.github.com>
2025-06-19 17:29:55 +0000

    Delete lua directory

--- lua/plugins/ultisnips.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "SirVer/ultisnips",
-  config = function()
-    vim.g.UltiSnipsExpandTrigger = "<tab>"
-    vim.g.UltiSnipsJumpForwardTrigger = "<c-b>"
-    vim.g.UltiSnipsJumpBackwardTrigger = "<c-z>"
-  end,
-}
